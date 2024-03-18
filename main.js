6e61c1be16ccec9a506102fbe90a0ea87bb14908
cnhjp <tonyhjp888@gmail.com>
2024-03-18 10:24:55 +0800

    🌈 style(默认窗口大小):

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const { app, BrowserWindow, ipcMain } = require("electron/main");
+const { app, BrowserWindow, ipcMain, screen } = require("electron/main");
 const path = require("node:path");
 const { updateElectronApp } = require("update-electron-app");
 
+// 自动更新
 updateElectronApp();
 
 const createWindow = () => {
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const { width, height } = primaryDisplay.workAreaSize;
+
   const win = new BrowserWindow({
     // 运行时的图标
     icon: path.join(__dirname, "image", "icon.png"),
-    width: 800,
-    height: 600,
+    width: width * 0.7,
+    height: height * 0.7,
     webPreferences: {
       preload: path.join(__dirname, "js", "preload.js"),
       nodeIntegration: true,
